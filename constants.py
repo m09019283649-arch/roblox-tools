8abec0f114839caf3b0d2e23912b2cbe37d46a00
m0arch.dev <m09019283649@gmail.com>
2026-04-19 04:30:24 +0100

    add consolidate constants into a single file

--- constants.py
+++ constants.py
@@ -1,39 +1,50 @@
-BASE_URL = 'https://api.roblox.com/'
+GAME_ID = 123456789
 
-ROLES = {
-    'admin': 1,
-    'moderator': 2,
-    'user': 3,
+MAX_PLAYERS = 30
+
+SERVER_NAME = 'MyRobloxServer'
+
+SPECIAL_ITEMS = {
+    'GoldSword': {
+        'value': 100,
+        'type': 'weapon',
+        'description': 'A shiny golden sword!'
+    },
+    'MagicPotion': {
+        'value': 50,
+        'type': 'consumable',
+        'description': 'A potion that restores health!'
+    }
 }
 
-GAME_INFO_KEYS = [
-    'name',
-    'id',
-    'creator',
-    'playerCount',
-    'maxPlayers',
-    'description',
+DEFAULT_CONFIG = {
+    'gameMode': 'adventure',
+    'difficulty': 'normal',
+    'maxTime': 120,
+    'enableChat': True
+}
+
+ITEM_COLORS = {
+    'GoldSword': '#FFD700',
+    'MagicPotion': '#8A2BE2'
+}
+
+PLAYER_ROLES = [
+    'admin',
+    'moderator',
+    'player'
 ]
 
-ITEM_TYPES = {
-    'clothing': 1,
-    'accessory': 2,
-    'gear': 3,
-    'model': 4,
-}
+REGION_NAMES = [
+    'Town',
+    'Forest',
+    'Dungeon'
+]
 
-def get_role_name(role_id):
-    for role, id in ROLES.items():
-        if id == role_id:
-            return role
-    return 'unknown'
+DISCORD_WEBHOOK_URL = 'https://discord.com/api/webhooks/your_webhook_here'
 
-def get_game_info_key(index):
-    try:
-        return GAME_INFO_KEYS[index]
-    except IndexError:
-        return 'invalid_key'
-
-def is_item_type(item_type):
-    return item_type in ITEM_TYPES.keys()
-
+WINNING_CONDITIONS = [
+    'collect_all_items',
+    'defeat_boss',
+    'complete_time_challenge'
+]
